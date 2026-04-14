3281d6a44d3cbef9e925cb306a56ec2b0c537475
Vidyasagar29 <138607620+Vidyasagar29@users.noreply.github.com>
2026-04-14 22:59:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  EDGE_FUNCTION_URL: "https://YOUR_PROJECT_REF.supabase.co/functions/v1/breeze-token"
+  EDGE_FUNCTION_URL: "https://znxgfmixavgfdrisbcnt.supabase.co/functions/v1/breeze-token"
 };
